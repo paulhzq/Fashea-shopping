31d3a0b27a696918771e8784ba697b0ba1925331
Paul Huang <paul_hzq@hotmail.com>
2017-06-20 17:32:50 -0400

    init the project

---
+++ shop/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url,include
+from . import views
+from django.contrib.auth.views import login,logout
+
+urlpatterns = [
+    url(r'^$', views.product_list, name='product_list'),
+    url(r'^login/$',login,name="login"),
+    url(r'^logout/$',logout, {'next_page': '/'}, name="logout"),
+    url(r'^(?P<category_slug>[-\w]+)/$',
+        views.product_list,
+        name='product_list_by_category'),
+    url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$',
+        views.product_detail,
+        name='product_detail'),
+]
